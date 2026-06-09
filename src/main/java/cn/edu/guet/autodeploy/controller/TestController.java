3017884db5f53d5d8283512c842829ae56009421
吴宗煌 <2118230590@qq.com>
2026-06-09 20:26:50 +0800

    wu

---
+++ src/main/java/cn/edu/guet/autodeploy/controller/TestController.java
@@ -0,0 +1,15 @@
+package cn.edu.guet.autodeploy.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class TestController {
+    @GetMapping("/auto")
+    public String testAutoDeploy(){
+        return "部署成功";
+    }
+}
