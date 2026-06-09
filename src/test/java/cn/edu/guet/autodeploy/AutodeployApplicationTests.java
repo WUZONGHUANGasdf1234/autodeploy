3534ffb067d4314513c2bf8e52f46d499190c1e7
吴宗煌 <2118230590@qq.com>
2026-06-09 20:26:41 +0800

    wu

---
+++ src/test/java/cn/edu/guet/autodeploy/AutodeployApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.autodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
